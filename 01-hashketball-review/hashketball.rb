d2f8cce97f1a425fa10855cfdfa28838567efd54
Michael Cheng <michael.cheng@flatironschool.com>
2019-01-07 16:10:34 -0500

    halfway

--- 01-hashketball-review/hashketball.rb
+++ 01-hashketball-review/hashketball.rb
@@ -1,12 +1,19 @@
-require 'pry'
+require 'pry' # just pulls in the pry code
 
+# shortcuts!!!!!!!!
+
+# TDD => write your tests for what you want it to do
+# then write the code to pass those tests
+
+# CMD + /
 def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: {
-        "Alan Anderson" => {
+      players: [
+        {
+          player_name: "Alan Anderson",
           number: 0,
           shoe: 16,
           points: 22,
@@ -15,8 +22,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
-        },
-        "Reggie Evans" => {
+        }, {
+          player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
           points: 12,
@@ -25,8 +32,8 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7
-        },
-        "Brook Lopez" => {
+        }, {
+          player_name: "Brook Lopez",
           number: 11,
           shoe: 17,
           points: 17,
@@ -35,8 +42,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
-        },
-        "Mason Plumlee" => {
+        }, {
+          player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
           points: 26,
@@ -45,8 +52,8 @@ def game_hash
           steals: 3,
           blocks: 8,
           slam_dunks: 5
-        },
-        "Jason Terry" => {
+        }, {
+          player_name: "Jason Terry",
           number: 31,
           shoe: 15,
           points: 19,
@@ -56,13 +63,14 @@ def game_hash
           blocks: 11,
           slam_dunks: 1
         }
-      }
+      ]
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players: {
-        "Jeff Adrien" => {
+      players: [
+        {
+          player_name: "Jeff Adrien",
           number: 4,
           shoe: 18,
           points: 10,
@@ -71,8 +79,8 @@ def game_hash
           steals: 2,
           blocks: 7,
           slam_dunks: 2
-        },
-        "Bismak Biyombo" => {
+        }, {
+          player_name: "Bismak Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
@@ -81,8 +89,8 @@ def game_hash
           steals: 7,
           blocks: 15,
           slam_dunks: 10
-        },
-        "DeSagna Diop" => {
+        }, {
+          player_name: "DeSagna Diop",
           number: 2,
           shoe: 14,
           points: 24,
@@ -91,8 +99,8 @@ def game_hash
           steals: 4,
           blocks: 5,
           slam_dunks: 5
-        },
-        "Ben Gordon" => {
+        }, {
+          player_name: "Ben Gordon",
           number: 8,
           shoe: 15,
           points: 33,
@@ -101,8 +109,8 @@ def game_hash
           steals: 1,
           blocks: 1,
           slam_dunks: 0
-        },
-        "Brendan Haywood" => {
+        }, {
+          player_name: "Brendan Haywood",
           number: 33,
           shoe: 15,
           points: 6,
@@ -112,126 +120,44 @@ def game_hash
           blocks: 5,
           slam_dunks: 12
         }
-      }
+      ]
     }
   }
 end
 
-# def game_hash
-#   {
-#     home: {
-#       team_name: "Brooklyn Nets",
-#       colors: ["Black", "White"],
-#       players: [
-#         {
-#           player_name: "Alan Anderson",
-#           number: 0,
-#           shoe: 16,
-#           points: 22,
-#           rebounds: 12,
-#           assists: 12,
-#           steals: 3,
-#           blocks: 1,
-#           slam_dunks: 1
-#         }, {
-#           player_name: "Reggie Evans",
-#           number: 30,
-#           shoe: 14,
-#           points: 12,
-#           rebounds: 12,
-#           assists: 12,
-#           steals: 12,
-#           blocks: 12,
-#           slam_dunks: 7
-#         }, {
-#           player_name: "Brook Lopez",
-#           number: 11,
-#           shoe: 17,
-#           points: 17,
-#           rebounds: 19,
-#           assists: 10,
-#           steals: 3,
-#           blocks: 1,
-#           slam_dunks: 15
-#         }, {
-#           player_name: "Mason Plumlee",
-#           number: 1,
-#           shoe: 19,
-#           points: 26,
-#           rebounds: 12,
-#           assists: 6,
-#           steals: 3,
-#           blocks: 8,
-#           slam_dunks: 5
-#         }, {
-#           player_name: "Jason Terry",
-#           number: 31,
-#           shoe: 15,
-#           points: 19,
-#           rebounds: 2,
-#           assists: 2,
-#           steals: 4,
-#           blocks: 11,
-#           slam_dunks: 1
-#         }
-#       ]
-#     },
-#     away: {
-#       team_name: "Charlotte Hornets",
-#       colors: ["Turquoise", "Purple"],
-#       players: [
-#         {
-#           player_name: "Jeff Adrien",
-#           number: 4,
-#           shoe: 18,
-#           points: 10,
-#           rebounds: 1,
-#           assists: 1,
-#           steals: 2,
-#           blocks: 7,
-#           slam_dunks: 2
-#         }, {
-#           player_name: "Bismak Biyombo",
-#           number: 0,
-#           shoe: 16,
-#           points: 12,
-#           rebounds: 4,
-#           assists: 7,
-#           steals: 7,
-#           blocks: 15,
-#           slam_dunks: 10
-#         }, {
-#           player_name: "DeSagna Diop",
-#           number: 2,
-#           shoe: 14,
-#           points: 24,
-#           rebounds: 12,
-#           assists: 12,
-#           steals: 4,
-#           blocks: 5,
-#           slam_dunks: 5
-#         }, {
-#           player_name: "Ben Gordon",
-#           number: 8,
-#           shoe: 15,
-#           points: 33,
-#           rebounds: 3,
-#           assists: 2,
-#           steals: 1,
-#           blocks: 1,
-#           slam_dunks: 0
-#         }, {
-#           player_name: "Brendan Haywood",
-#           number: 33,
-#           shoe: 15,
-#           points: 6,
-#           rebounds: 12,
-#           assists: 12,
-#           steals: 22,
-#           blocks: 5,
-#           slam_dunks: 12
-#         }
-#       ]
-#     }
-#   }
-# end
+# to get an array of all the players together
+# create a new array
+# shovel in game_hash[:home][:players] to the new array
+# shovel in game_hash[:away][:players] to the new array
+# flatten <== another step
+
+# use something like select or find to find the key players
+# refactor
+
+# merge the two arrays of players => hash
+
+# iteration where you go from the top to the bottom
+# then if what's in your pipe, then do the same thing in
+# the first solution
+# => iterate through the player
+# then shovel them into a new array
+
+# do whatever works!
+
+# Process => ok let me thing in high level steps
+# Pseudocode
+# new array for all the players
+# add in the home players
+# add in the away players
+# then flatten it
+
+# get the home players and add it to the away players
+  # home players
+home_players = game_hash[:home][:players]
+  # away players
+away_players = game_hash[:away][:players]
+  # then add them ()
+# all_players = home_players.concat(away_players)
+all_players = home_players + away_players
+
+# binding.pry
